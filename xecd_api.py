6a4b29c850e662adbb50665aa99a3412ac8ba0d9
Sam Liew <samliew211@outlook.com>
2019-09-14 19:25:33 -0400

    Create xe api (#3)

---
+++ xecd_api.py
@@ -0,0 +1,9 @@
+from xecd_rates_client import XecdClient
+# install using pip install xecd-rates-client or pip3 install xecd-rates-client
+xecd = XecdClient('xe891090202', '8ht3qjhq8vnu0dbb91ai40p44b')
+
+def usd_to(currency: str):
+    return xecd.convert_from("USD", currency)['to'][0]['mid']
+
+def cad_to(currency: str):
+    return xecd.convert_from("CAD", currency)['to'][0]['mid']
