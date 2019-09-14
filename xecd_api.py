e19e57e6abd8ee84fa56f4004964344197e6dbd5
liew211 <samliew211@outlook.com>
2019-09-14 19:22:42 -0400

    Change to functions

--- xecd_api.py
+++ xecd_api.py
@@ -1,4 +1,8 @@
 from xecd_rates_client import XecdClient
 xecd = XecdClient('xe891090202', '8ht3qjhq8vnu0dbb91ai40p44b')
-usd_to_cad=xecd.convert_from("USD", "CAD")['to'][0]['mid']
-cad_to_usd=xecd.convert_from("CAD", "USD")['to'][0]['mid']
+
+def usd_to(currency: str):
+    return xecd.convert_from("USD", currency)['to'][0]['mid']
+
+def cad_to(currency: str):
+    return xecd.convert_from("CAD", currency)['to'][0]['mid']
