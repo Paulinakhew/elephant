57e83e38e7491e846a9136e4fc4ad99d6af5737f
Stefan Vercillo <svercillo7@gmail.com>
2019-09-14 18:58:15 -0400

    retrieval of customer id

--- retrieval.py
+++ retrieval.py
@@ -5,9 +5,10 @@ response = requests.post('https://api.td-davinci.com/api/raw-customer-data',
     headers = { 'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJDQlAiLCJ0ZWFtX2lkIjoiNjljZDEyYmItMDlkYy0zNjc4LWJlNWYtZGExNmNjY2U3MjRjIiwiZXhwIjo5MjIzMzcyMDM2ODU0Nzc1LCJhcHBfaWQiOiIwYWVjMTI3ZS1hZmU0LTQ3ZWItOTQ1My0zOTM4MjRlNjJjMjQifQ.thFjT6PaKBJ7XT4IYXWNsHeT_rlsXoNY5oYb0uQH5JA' })
 response_data = response.json()
 
-customer_id = []
+cust_id = []
 
-for r in response_data['result']['customers']['id']:
-    print(r)
+for i in range(0,1000):
+    response_data['result']['customers'][i]['id']
+
 
 # print(response_data['result'])
